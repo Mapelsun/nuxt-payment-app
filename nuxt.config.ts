c8f0ffd3a0cc7221b5f42120e67d1c846d0e5e71
Pels <pelumisunday@gmail.com>
2023-09-26 21:57:48 +0100

    build: add nuxt3-leaflet as dep

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', 'nuxt3-leaflet'],
   colorMode: {
-    preference: 'light'
+    preference: 'light',
   },
   ui: {
-    icons: 'all'
-  }
+    icons: 'all',
+  },
 })
+
