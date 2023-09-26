2402552f7082fe1f4371e574c60af50cc9cb43ea
Pels <pelumisunday@gmail.com>
2023-09-26 07:12:28 +0100

    feat: create fixed navbar comp

---
+++ components/NavBar.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+  <div
+    class="fixed bg-white bottom-0 left-0 right-0 border-t flex items-center justify-between p-4 [&_a]:flex [&_a]:flex-col [&_a]:items-center [&_span]:text-xs">
+    <NuxtLink to="/chats">
+      <Icon name="i-carbon-chat" />
+      <span>Chats</span>
+    </NuxtLink>
+    <NuxtLink to="/schedule">
+      <Icon name="i-teenyicons-calculator-outline" />
+      <span>Schedule</span>
+    </NuxtLink>
+    <NuxtLink to="/pickups">
+      <Icon name="i-oi-map-marker" />
+      <span>Pickups</span>
+    </NuxtLink>
+    <NuxtLink to="/earnings">
+      <Icon name="i-la-coins" />
+      <span>Earnings</span>
+    </NuxtLink>
+    <NuxtLink to="/account">
+      <Icon name="i-tdesign-user" />
+      <span>Account</span>
+    </NuxtLink>
+  </div>
+</template>
